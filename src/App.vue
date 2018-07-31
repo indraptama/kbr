d77abfbc91c9a3be954c3d1e3060bf66520a60e9
Indra Pratama Putra <indra.symfonia@gmail.com>
2018-07-31 09:53:43 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  #app
+    <MainHeader />
+    h1.fira {{msg}}
+</template>
+
+<script>
+
+import MainHeader from "./Components/MainHeader.vue";
+
+export default {
+  name: 'app',
+  components: {
+    MainHeader
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js Appss!'
+    }
+  }
+}
+</script>
+
---
+++ src/Components/MainHeader.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div
+    header(class="MainHeader bg-reds")
+      nav(class="mx-auto flex items-center justify-start bg-white px-4")
+        a(href="#" class="inline-block py-2 px-4 no-underline text-grey-dark hover:text-black uppercase tracking-wide text-sm fira font-normal") About Us
+        a(href="#" class="inline-block py-2 px-4 no-underline text-grey-dark hover:text-black uppercase tracking-wide text-sm fira font-normal") Pedoman Media Siber
+        a(href="#" class="inline-block py-2 px-4 no-underline text-grey-dark hover:text-black uppercase tracking-wide text-sm fira font-normal") Jaringan
+        a(href="#" class="inline-block py-2 px-4 no-underline text-grey-dark hover:text-black uppercase tracking-wide text-sm fira font-normal") Contact Us
+        a(href="#" class="inline-block py-2 px-4 no-underline text-grey-dark hover:text-black uppercase tracking-wide text-sm fira font-normal") Kbr Pagi
+      div(class="max-w-3xl mx-auto px-4")
+        div(class="logo")
+          img(src="../Assets/logo.png" class="shadow")
+      nav(class="MainNav mx-auto flex items-center justify-center border-t border-b bg-white px-4")
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Berita
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Saga
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Indonesia 2019
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Asia Calling
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Kbr Prime
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Ruang Publik
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Kabar Bisnis
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Opini
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Gallery
+        a(href="#" class="inline-block py-2 px-4 no-underline border-l text-grey-dark hover:text-black uppercase tracking-wide xl:text-lg fira font-normal") Search
+
+</template>
+
+<script>
+  
+  export default {
+    name: 'MainHeader'
+  }
+</script>
